d8a2d60062c9eb75ddfec0efd6aa5cbdfd8cb26a
Kenneth Ejiofor <ejioforkenneth08@gmail.com>
2023-05-19 19:52:39 -0700

    My commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write (2, message, strlen(message));
-    return (1);
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write (2, message, strlen(message));
+	return (1);
 } 
