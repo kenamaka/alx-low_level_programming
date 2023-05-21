814c2d63136da5f211055786a45ada050f4ddbaa
Kenneth Ejiofor <ejioforkenneth08@gmail.com>
2023-05-21 11:40:10 -0700

    Edited

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,12 +3,13 @@
 #include <string.h>
 
 /**
- * main - Prints the alphabet without q and e
+ * main - Prints the alphabet without 'q' and 'e'
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
 	int i;
+
 	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
@@ -16,6 +17,8 @@ int main(void)
 			putchar(i);
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,18 +2,17 @@
 
 /**
  * main - Prints numbers from 0 to 9.
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d", i)
-		
+		printf("%d", i);
 	}
-	putChar('\n');
+
+	putchar('\n');
 	return (0);
-} 
+}
