94277dff3dc09960304909a1dee568cbd6c3f35b
Kenneth Ejiofor <ejioforkenneth08@gmail.com>
2023-05-21 11:05:29 -0700

    Edited

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,21 +4,18 @@
 
 /**
  * main - Prints the alphabet without q and e
- * 
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	int i;
-	
 	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
 		{
-			putChar(i);
+			putchar(i);
 		}
-		
 	}
-	putChar('\n');
+	putchar('\n');
 	return (0);
-} 
+}
