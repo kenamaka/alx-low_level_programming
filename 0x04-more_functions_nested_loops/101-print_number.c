4474e87db86d53115ad7b53c6c386b7674c1d004
Kenneth Ejiofor <ejioforkenneth08@gmail.com>
2023-05-26 00:59:30 -0700

    My commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int nl;
+
+	if (n < 0)
+	{
+		nl = -n;
+		_putchar('-');
+	}
+	else
+	{
+		nl = n;
+	}
+
+	if (nl / 10)
+	{
+		print_number(nl / 10);
+	}
+
+	_putchar((nl % 10) + '0');
+}
